4f5c0c8cfb0ebd4212e052b87585e12c817a4c9b
fox <fox@360.com>
2025-09-18 09:56:25 +0800

    静态资源配置和使用

--- HelloWorld/HelloWorld/settings.py
+++ HelloWorld/HelloWorld/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 # 静态文件 URL（CSS/JS/图片）
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
---
+++ HelloWorld/static/js/runoob.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(() => {
+        alert('这是外部引入的脚本被执行。。。')
+    },3000)
+}
